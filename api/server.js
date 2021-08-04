4756f8c563f3f45f7dc51ed2d29a91012dc5771a
amanuel gilay <amanuelgilay@amanuels-MacBook-Air.local>
2021-08-03 20:28:13 -0700

    finished node-api1 project

--- api/server.js
+++ api/server.js
@@ -1,77 +1,123 @@
 // BUILD YOUR SERVER HERE
 const express = require ('express')
-
-const user = require ('./users')
-
+const user = require ('./users/model')
 const server = express()
 
-server.use(express.Json)
+
+server.put('api/user:id',async (req, res) => {
+    try {
+    const posibleUser = await user.findById(req.params.id)
+    if(posibleUser) {
+        res.status(200).json ({
+            message: 'The user with the specified ID does not exist',
+        })
+     }else {
+        if(!req.body.name || req.body.bio) {
+            res.status(404).json({
+                message: 'please provide name and bio for the user',
+            })
+        }else {
+            const updatedUser = await User.update(
+                req.params.id,
+                req.body,
+            )
+            res.status(200).json(updatedUser)
+        }
+    }
+
+} catch(err) {
+    res.status(500).json({
+        message: 'err updating user',
+        err: err.message,
+        stack: err.stack
+    })
+}
+
+})
 
 
+
+server.delete('api/users:id', async (req, res) => {
+   try {
+    const posibleUser = await user.findById(req.params.id)
+    if(posibleUser) {
+        res.status(200).json ({
+            message: 'The user with the specified ID does not exist'
+        })
+    }else {
+        const deleteUser = await User.remove(posibleUser.id)
+        res.status(200).json(deleteUser)
+    }
+   }catch(err) {
+    res.status(500).json({
+        message: 'error deleting user',
+        err: err.message,
+        stack:err.stack
+    })
+
+   }
+})
 
 server.post ('/api/users', (reg, res) => {
-    const { name, bio } = req.bio
-    newUser.create ({ name, bio })
-           .then( user => {
-               res.status(201).Json(user)
-           })
+    const user = req.body;
+    if(!user.name || !user.bio) {
+        res.status(400).json({
+            message: 'please provide name and bio for the user',
+        })
+    } else {
+        user.insert(user)
+            .then(createUser =>{
+                res.status(201).json(createUser)
+            })
+            .catch(err=>{
+                res.status(500).json({
+                    message: 'error creating user',
+                    err: err.message,
+                    stack:err.stack
+                })
+            })
+    }
 
-           .catch(err => {
-            res.status(500).json({ message: err.message })
-          })
+          
 })
 
 
+server.get('/api/users', (req, res)=>{
+    user.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err=>{
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+                stack:err.stack
+            })
+        })
+})
 
 server.get ('api/users/:id', (req, res) => {
-    const { id }= req.params
-
-    user.findById(id)
-           .then(user => {
-               if(!user) {
-                   res.status (404).json ({ message: `user ${id} not found`})
-               }else {
-                res.status(200).json(user)
-               }
+   user.findById(req.params.id)
+          .then(user => {
+             res.json(user)
+          })
+          .catch(err=>{
+           res.status(500).json({
+               message: 'error getting user',
+               err: err.message,
+               stack:err.stack
            })
+       })
 })
 
 
 
-server.delete('api/users/:id', (req, res) => {
-    const { id } = req.params
-    console.log('about to delete', id)
-    // in the afternoon you'll have to getById the dog before
-    // proceeding with the deletion!
-    user.delete(id)
-      .then(deleted => {
-        if (!deleted) {
-          res.status(404).json({ message: 'not found!' })
-        } else {
-          res.json(deleted)
-        }
-      })
-      .catch(err => {
-        res.status(500).json({ message: err.message })
-      })
-  })
+server.use('*',(req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
 
-
-  server.put('api/users/:id', (req, res) => {
-    const { id } = req.params
-    const { name, bio } = req.body
-    user.update(id, { name, bio })
-      .then(updatedDog => {
-        if (!updatedUser) {
-          res.status(404).json({ message: 'not found!' })
-        } else {
-          res.json(updatedUser) // defaults to status 200
-        }
-      })
-      .catch(err => {
-        res.status(500).json({ message: err.message })
-      })
-  })
 
 
 module.exports = server // EXPORT YOUR SERVER instead of {}
